954121725ba0110d864ddf0f80455d1e64d18d27
Karthik S <81423983+Karthik-02@users.noreply.github.com>
2023-06-16 01:51:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { ObjectId } = require('mongodb');
 const { connect, getDB } = require('./db');
 
@@ -6,6 +7,8 @@ const app = express();
 const port = 3000; // Replace with your desired port number
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/api/v3/app/events', async (req, res) => {
   const { id, type, limit, page } = req.query;
@@ -15,7 +18,7 @@ app.get('/api/v3/app/events', async (req, res) => {
     let query = {};
 
     if (id) {
-      query = { _id: ObjectId(id) };
+      query = { _id: new ObjectId(id) };
     } else if (type === 'latest') {
       const skip = (page - 1) * limit;
       query = { type: 'event' };
@@ -23,13 +26,14 @@ app.get('/api/v3/app/events', async (req, res) => {
         .find(query)
         .sort({ schedule: -1 })
         .skip(skip)
-        .limit(limit)
+        .limit(parseInt(limit))
         .toArray();
 
       return res.json(events);
     }
 
     const event = await db.collection('events').findOne(query);
+    res.setHeader('Content-Type', 'application/json'); 
     res.json(event);
   } catch (error) {
     console.error('Error retrieving event:', error);
@@ -37,55 +41,18 @@ app.get('/api/v3/app/events', async (req, res) => {
   }
 });
 
-const sampleData = [
-  {
-    type: 'event',
-    uid: 18,
-    name: 'Sample Event 1',
-    tagline: 'A proper tagline for Sample Event 1',
-    schedule: new Date('2023-06-16T09:00:00Z'),
-    description: 'Sample description for Sample Event 1',
-    files: {
-      image: 'sample_image1.jpg',
-    },
-    moderator: 'John Doe',
-    category: 'Category 1',
-    sub_category: 'Subcategory 1',
-    rigor_rank: 3,
-    attendees: [1, 2, 3],
-  },
-  {
-    type: 'event',
-    uid: 18,
-    name: 'Sample Event 2',
-    tagline: 'A proper tagline for Sample Event 2',
-    schedule: new Date('2023-06-17T14:30:00Z'),
-    description: 'Sample description for Sample Event 2',
-    files: {
-      image: 'sample_image2.jpg',
-    },
-    moderator: 'Jane Smith',
-    category: 'Category 2',
-    sub_category: 'Subcategory 2',
-    rigor_rank: 4,
-    attendees: [4, 5, 6],
-  },
-];
-
-
 app.post('/api/v3/app/events', async (req, res) => {
   const event = req.body;
 
   try {
     const db = getDB();
-    const result = await db.collection('events').insertMany(sampleData);
-    res.json({ ids: result.insertedIds });
+    const result = await db.collection('events').insertOne(event);
+    res.json({ id: result.insertedId });
   } catch (error) {
     console.error('Error creating event:', error);
     res.status(500).json({ error: 'An error occurred' });
   }
 });
-
 
 app.put('/api/v3/app/events/:id', async (req, res) => {
   const { id } = req.params;
@@ -93,7 +60,7 @@ app.put('/api/v3/app/events/:id', async (req, res) => {
 
   try {
     const db = getDB();
-    const result = await db.collection('events').updateOne({ _id: ObjectId(id) }, { $set: event });
+    const result = await db.collection('events').updateOne({ _id: new ObjectId(id) }, { $set: event });
     res.json({ modifiedCount: result.modifiedCount });
   } catch (error) {
     console.error('Error updating event:', error);
@@ -101,12 +68,13 @@ app.put('/api/v3/app/events/:id', async (req, res) => {
   }
 });
 
+
 app.delete('/api/v3/app/events/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
     const db = getDB();
-    const result = await db.collection('events').deleteOne({ _id: ObjectId(id) });
+    const result = await db.collection('events').deleteOne({ _id: new ObjectId(id) });
     res.json({ deletedCount: result.deletedCount });
   } catch (error) {
     console.error('Error deleting event:', error);
@@ -115,8 +83,9 @@ app.delete('/api/v3/app/events/:id', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Server is running'); // Change the response message as needed
+  res.send('Server is running'); 
 });
+
 
 // Start the server
 connect().then(() => {
